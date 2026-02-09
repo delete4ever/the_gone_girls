aa8c0ad4b234308ea15c467a71aaaa529c2c9c9e
delete4ever <lululudi.yang@outlook.com>
2026-02-09 18:46:06 +0800

    Fix: Remove undefined from links

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   // Automatically redirects localhost:3000 -> localhost:3000/projects/gone-girls/introduction
-  redirect('/projects/gone-girls/introduction');
+  redirect('/projects/introduction');
 }
